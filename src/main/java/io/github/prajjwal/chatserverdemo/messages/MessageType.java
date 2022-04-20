10236eb1802fbc1400d7a0f098e3b6c745271edb
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-04-20 23:53:03 +0530

    Create a enum of message type

---
+++ src/main/java/io/github/prajjwal/chatserverdemo/messages/MessageType.java
@@ -0,0 +1,11 @@
+package io.github.prajjwal.chatserverdemo.messages;
+
+public enum MessageType {
+    DISCONNECTED,
+    CONNECTED,
+    STATUS,
+    USER,
+    SERVER,
+    NOTIFICATION,
+    VOICE
+}
