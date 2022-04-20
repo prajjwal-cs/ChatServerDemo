7db10b0fd1c5717d469f77ca637e01e821a1d66f
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-04-21 00:18:16 +0530

    refactor

---
+++ src/main/java/io/github/prajjwal/chatserverdemo/controller/ServerController.java
@@ -0,0 +1,21 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 21-04-2022
+Time: 12:10 AM
+File: ServerController.java */
+package io.github.prajjwal.chatserverdemo.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ServerController {
+    @FXML
+    private Label serverStartText;
+
+    @FXML
+    private Label statusText;
+
+    @FXML
+    protected void onStartButtonClick() {}
+}
---
+++ src/main/java/io/github/prajjwal/chatserverdemo/server/Server.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 21-04-2022
+Time: 12:12 AM
+File: Server.java */
+package io.github.prajjwal.chatserverdemo.server;
+
+public class Server {
+}
